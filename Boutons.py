b88897229f48f0ad448a7974be01a85b0a3480ce
baptistefillie-santin <b.filliesantin@icloud.com>
2024-03-10 16:00:01 +0100

    réorganisation plus lisible du code

--- Boutons.py
+++ Boutons.py
@@ -15,34 +15,8 @@ class Button:
         text_rect = text_surface.get_rect(center=self.rect.center)
         surface.blit(text_surface, text_rect)
 
-    def is_hovered(self, pos):
-        return self.rect.collidepoint(pos)
-
-    def perform_action(self):
-        if self.action:
-            self.action()
-
-def solo_action():
-    print("Mode Solo")
-
-def one_vs_one_action():
-    print("1v1 Mode")
-
-def settings_action():
-    print("Paramètres")
-
-def return_to_menu_action():
-    print("Retour au menu principal")
-
-def create_buttons():
+def create_buttons(x, y, width, height, text, color, hover_color, ):
     # Définir les polices localement
     font_large = pygame.font.SysFont("arialblack", 40)
-    font_small = pygame.font.SysFont("arialblack", 30)
-
-    solo_button = Button(400, 500, 150, 50, "Solo", font_large, (0, 128, 255), (0, 100, 200), solo_action)
-    one_vs_one_button = Button(640, 500, 150, 50, "1v1", font_large, (0, 128, 0), (0, 100, 0), one_vs_one_action)
-    settings_button = Button(1000, 500, 200, 50, "Paramètres", font_small, (255, 165, 0), (255, 140, 0), settings_action)
-
-    buttons = [solo_button, one_vs_one_button, settings_button]
-
-    return buttons
+    boutons = Button(x, y, width, height, text, font_large, color, hover_color, action=None)
+    return boutons
--- main.py
+++ main.py
@@ -1,56 +1,47 @@
 import pygame
 import time
-from Boutons import*
+from Boutons import *
 
 pygame.init()
 
-# Créer la fenêtre de jeu
-LARGEUR_ECRAN = 1280
-HAUTEUR_ECRAN = 720
-
-ecran = pygame.display.set_mode((LARGEUR_ECRAN, HAUTEUR_ECRAN))
+#créer la fenêtre du jeu :
+ecran = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Ultimate Archer")
 
-# Charger l'image de fond
-image_fond = pygame.image.load("Images\Intro-fond.jpg").convert()
-image_fond = pygame.transform.scale(image_fond, (LARGEUR_ECRAN, HAUTEUR_ECRAN))
+#charger l'image de fond
+image_fond = pygame.image.load("Images/Intro-fond.jpg").convert()
+image_fond = pygame.transform.scale(image_fond, (1280, 720))
 
 # Définir les polices
 police_large = pygame.font.SysFont("arialblack", 60)
 police_petite = pygame.font.SysFont("arialblack", 40)
-# Définir les couleurs
-COULEUR_TEXTE = (255, 255, 255)
 
 def text(texte, police, couleur_texte, x, y):
     image_texte = police.render(texte, True, couleur_texte)
     ecran.blit(image_texte, (x, y))
 
-def main():
+run = True
+
+#Boucle tant que la condition est vrai
+while run :
 
     horloge = pygame.time.Clock()
-    run = True
-    buttons = create_buttons()
 
-    while run:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                for button in buttons:
-                    if button.is_hovered(pygame.mouse.get_pos()):
-                        button.perform_action()
+    ecran.blit(image_fond, (0, 0))  # Fond d'écran
+    text("ULTIMATE ARCHER", police_large, (255, 255, 255), 310, 120)
+    text("Menu Principal", police_petite, (255, 255, 255), 470, 220)
 
-        horloge.tick(60)
-        #Intro
-        ecran.blit(image_fond, (0, 0))  #Fond d'écran
-        text("ULTIMATE ARCHER", police_large, COULEUR_TEXTE, 310, 120)
-        text("Menu Principal", police_petite, COULEUR_TEXTE, 470, 220)
-        for button in buttons:
-            button.draw(ecran)
+    solo_button = create_buttons(550, 300, 150, 50, "Solo", (0, 128, 255), (0, 100, 200))
+    solo_button.draw(ecran)
+    one_vs_one_button = create_buttons(550, 400, 150, 50, "1v1",(0, 128, 0), (0, 100, 0))
+    one_vs_one_button.draw(ecran)
+    settings_button = create_buttons(500, 500, 260, 50, "Paramètres", (255, 165, 0), (255, 140, 0))
+    settings_button.draw(ecran)
 
-        pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            pygame.quit()
 
-if __name__ == "__main__":
-    main()
+    pygame.display.update()
 
-pygame.quit()
